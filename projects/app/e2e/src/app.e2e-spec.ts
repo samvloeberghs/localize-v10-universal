068e7dcd6cb072759f32d70148ed5f1ec1c80392
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-06-30 22:09:17 +0200

    tmp 1

--- projects/app/e2e/src/app.e2e-spec.ts
+++ projects/app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('localize-v10-universal app is running!');
+    expect(page.getTitleText()).toEqual('app app is running!');
   });
 
   afterEach(async () => {
--- projects/app/src/app/app.component.spec.ts
+++ projects/app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'localize-v10-universal'`, () => {
+  it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('localize-v10-universal');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('localize-v10-universal app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
   });
 });
